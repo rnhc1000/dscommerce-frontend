0fdd656c5a3ca801c3b091cfdf80624a2ad77c64
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-10-18 17:23:21 -0300

    feat(axios.withCredentials) - Allow true/false credentials

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,12 +1,20 @@
+import { AxiosRequestConfig } from "axios";
 import { requestBackEnd } from "../utils/requests";
-import * as authService from './auth-service';
+// import * as authService from './auth-service';
 
 export function findMe()  {
 
-    const headers = {
-        Authorization: "Bearer " + authService.getAccessToken()
+    // const headers = {
+    //     Authorization: "Bearer " + authService.getAccessToken()
+    // }
+    
+    const config: AxiosRequestConfig = {
+        url: "/users/me",
+        withCredentials: true
     }
-    console.log(headers);
 
-    return requestBackEnd({ url: `/users/me`, headers: headers }); 
+    // return requestBackEnd({ url: `/users/me`, headers: headers }); 
+    return requestBackEnd(config); 
 }
+
+
