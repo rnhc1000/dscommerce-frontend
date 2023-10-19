da8e7ddf2f322776f67d8219d9dfac31dad92706
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-10-19 18:45:25 -0300

    feat(ProductList) Paging thru the whole list of products

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -4,6 +4,8 @@ import deleteImg from '../../../assets/delete.svg';
 import { useEffect, useState } from 'react';
 import * as productService from '../../../services/product-service';
 import { ProductDTO } from '../../../models/product';
+import SearchBar from '../../../components/SearchBar';
+import LoadBar from '../../../components/LoadBar';
 
 type QueryParams = {
     page: number;
@@ -29,6 +31,15 @@ export default function ProductListing() {
             });
     }, [queryParams]);
 
+    function handleSearch(searchText: string) {
+        setProducts([]);
+        setQueryParams({ ...queryParams, page: 0, name: searchText });
+    }
+
+    function handlePageClick() {
+        setQueryParams({ ...queryParams, page: queryParams.page + 1 });
+    }
+
     return (
         <main>
             <section id="product-listing-section" className="dsc-container">
@@ -37,12 +48,7 @@ export default function ProductListing() {
                 <div className="dsc-btn-page-container dsc-mb20">
                     <div className="dsc-btn dsc-btn-white">Novo</div>
                 </div>
-
-                <form className="dsc-search-bar">
-                    <button type="submit">🔎︎</button>
-                    <input type="text" placeholder="Nome do produto" />
-                    <button type="reset">🗙</button>
-                </form>
+                <SearchBar onSearch={handleSearch} />
 
                 <table className="dsc-table dsc-mb20 dsc-mt20">
                     <thead>
@@ -58,7 +64,7 @@ export default function ProductListing() {
                     <tbody>
                         {
                             products.map(product => (
-                                <tr>
+                                <tr key={product.id}>
                                     <td className="dsc-tb576">{product.id}</td>
                                     <td><img className="dsc-product-listing-image" src={product.imgUrl} alt={product.name}></img></td>
                                     <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
@@ -70,8 +76,12 @@ export default function ProductListing() {
                         }
                     </tbody>
                 </table>
-
-                <div className="dsc-btn-next-page">Carregar mais</div>
+                {
+                    !isLastPage &&
+                    <div onClick={handlePageClick}>
+                        <LoadBar />
+                    </div>
+                }
             </section>
         </main>
     );
