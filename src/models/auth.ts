57b2cf8a54a6d2a5e76874383629799710e7f01c
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-10-19 11:19:32 -0300

    feat(isAuthenticated) Returns true if Authenticated (token Valid) ou false, otherwise

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -2,3 +2,11 @@ export type CredentialsDTO = {
     username: string,
     password: string
 }
+
+export type RoleEnum = "ROLE_ADMIN" | "ROLE_CLIENT";
+
+export type AccessTokenPayloadDTO = {
+    "exp": 1697809732,
+    user_name: string,
+    authorities: RoleEnum[]
+}
